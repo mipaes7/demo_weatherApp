dcc4721cb50c061ca321ed7cc174edd3174e8f53
Miguel Pardal <miguelpardalesparis@gmail.com>
2024-06-24 16:16:06 +0200

    styles functioning

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
     let city = req.body.city;
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
     request(url, function (err, response, body) {
         if (err) {
             res.render('index', { weather: null, error: 'Error, please try again' });
